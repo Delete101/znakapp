f4a3d028e57a8779884dd65b6cd9741d8f43fa00
Delete101 <102860660+Delete101@users.noreply.github.com>
2023-05-13 10:17:10 +0300

    Update settings.py

--- znakapp/znakapp/settings.py
+++ znakapp/znakapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%kj2@k^+yalfe-8sqcwjrg#j7fda%9phsrw*1fp-jg%%s)-99y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
